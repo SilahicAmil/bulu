923c9a596066f14ec3baa1815dcfb45269ce3dba
Amil Silahic <jinobie02@gmail.com>
2022-11-29 00:38:31 -0700

    side nav updates

--- components/SideNav/SideNav.js
+++ components/SideNav/SideNav.js
@@ -1,11 +1,12 @@
 const SideNav = (props) => {
   return (
     <>
-      <div className="w-60 h-full shadow-md bg-white px-1 absolute">
+      {/* wrap in card component type thingy */}
+      <div className="w-40 rounded-xl bg-[#d6be7a] h-full mt-15 shadow-md  px-1 absolute">
         <ul className="relative">
           <li className="relative">
             <a
-              className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+              className="flex text-white items-center text-sm py-4 px-6 h-12 overflow-hidden  text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
               href="#!"
               data-mdb-ripple="true"
               data-mdb-ripple-color="dark"
@@ -15,7 +16,7 @@ const SideNav = (props) => {
           </li>
           <li className="relative">
             <a
-              className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+              className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
               href="#!"
               data-mdb-ripple="true"
               data-mdb-ripple-color="dark"
@@ -25,7 +26,7 @@ const SideNav = (props) => {
           </li>
           <li className="relative">
             <a
-              className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+              className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
               href="#!"
               data-mdb-ripple="true"
               data-mdb-ripple-color="dark"
