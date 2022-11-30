ff718bcf201015590a87dc08955094019cac9e7b
Amil Silahic <jinobie02@gmail.com>
2022-11-30 16:06:22 -0700

    search API test

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,15 @@ import { useState } from "react";
 const BuluHomePage = () => {
   const [movies, setMovies] = useState([]);
 
-  const getMovieHandler = async () => {
+  const getMovieHandler = async (enteredText) => {
     // move API key in .env
     const response = await fetch(
-      "https://api.themoviedb.org/3/movie/550?api_key=ae0cecc2f7ae230acde5101cb7218ae9"
+      "https://api.themoviedb.org/3/search/movie?api_key=ae0cecc2f7ae230acde5101cb7218ae9&language=en-US&query=fight%20club&page=1&include_adult=false"
     );
     const data = await response.json();
-    console.log(data);
+    console.log(data.results[0]);
 
-    setMovies(data);
+    setMovies(data.results[0]);
   };
 
   return (
@@ -20,13 +20,15 @@ const BuluHomePage = () => {
         <h1 className="bg-white text-3xl text-black">Welcome to BULU</h1>
         <button onClick={getMovieHandler}>Get Data</button>
       </div>
-      {/* eventually pass all of these to card component */}
+      {/*  pass all of these to card component */}
       <div className="flex justify-center items-center">
         <div className="p-4">{<p>The Movie ID: {movies.id}</p>}</div>
         <div className=" p-4">
           {<p>The Movie Name: {movies.original_title}</p>}
         </div>
-        <div className=" p-4">{<p>The Movie Tagline: {movies.tagline}</p>}</div>
+        <div className=" p-4">
+          {<p>The Movie Popularity: {movies.popularity} %</p>}
+        </div>
       </div>
     </>
   );
