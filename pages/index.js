7d9a9912e89a3aded91f23c24377e8d9e1504742
Amil Silahic <jinobie02@gmail.com>
2022-11-30 10:44:08 -0700

    fianlly figured out what im doing

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,33 @@ const BuluHomePage = () => {
   //
   const [movies, setMovies] = useState([]);
 
-  const getMovieHandler = useCallback(async () => {
+  const getMovieHandler = async () => {
     const response = await fetch(
-      "https://api.themoviedb.org/3/movie/550?api_key=ae0cecc2f7ae230acde5101cb7218ae9",
-      { method: "GET", headers: { "Content-Type": "application/json" } }
+      "https://api.themoviedb.org/3/movie/550?api_key=ae0cecc2f7ae230acde5101cb7218ae9"
     );
     const data = await response.json();
     console.log(data);
-  }, []);
+
+    setMovies(data);
+  };
 
   return (
-    <div className="justify-center items-center flex">
-      <h1 className="bg-white text-3xl text-black">Welcome to BULU</h1>
-      <button onClick={getMovieHandler}>Get Data</button>
-      {/* {movies.map((item) => {
-        return <p key={item}>{item}</p>;
+    <>
+      <div className="justify-center items-center flex">
+        <h1 className="bg-white text-3xl text-black">Welcome to BULU</h1>
+        <button onClick={getMovieHandler}>Get Data</button>
+        {/* {movies.map((item) => {
+        return <p key={item}>{item.id}</p>;
       })} */}
-      {/* {movies} */}
-    </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="p-4">{<p>The Movie ID: {movies.id}</p>}</div>
+        <div className=" p-4">
+          {<p>The Movie Name: {movies.original_title}</p>}
+        </div>
+        <div className=" p-4">{<p>The Movie Tagline: {movies.tagline}</p>}</div>
+      </div>
+    </>
   );
 };
 
