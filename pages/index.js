afaab7e9369b1e5917811cf59e75a1b401f2ceca
Amil Silahic <jinobie02@gmail.com>
2022-11-30 16:07:49 -0700

    updates

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ const BuluHomePage = () => {
       "https://api.themoviedb.org/3/search/movie?api_key=ae0cecc2f7ae230acde5101cb7218ae9&language=en-US&query=fight%20club&page=1&include_adult=false"
     );
     const data = await response.json();
-    console.log(data.results[0]);
+    console.log(data.results);
 
-    setMovies(data.results[0]);
+    setMovies(data.results[1]);
   };
 
   return (
@@ -20,6 +20,7 @@ const BuluHomePage = () => {
         <h1 className="bg-white text-3xl text-black">Welcome to BULU</h1>
         <button onClick={getMovieHandler}>Get Data</button>
       </div>
+
       {/*  pass all of these to card component */}
       <div className="flex justify-center items-center">
         <div className="p-4">{<p>The Movie ID: {movies.id}</p>}</div>
