26fe94d8a708f9f7e9717bfb7823ae2a29e1e812
Amil Silahic <jinobie02@gmail.com>
2022-12-09 20:37:25 -0700

    comments

--- components/MovieItem/MovieItem.js
+++ components/MovieItem/MovieItem.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 const TvShowItem = ({ name, date, popularity }) => {
   //   probably need to useEffect to load the images
+  // probably use state here so nothing displays untul something is feteched
+  //
   //    or get it in the API call and pass it seperately
 
   const readableDate = new Date(date).toLocaleDateString("en-US");
@@ -13,8 +15,8 @@ const TvShowItem = ({ name, date, popularity }) => {
           <Image
             src="/felix-mooneeram-evlkOfkQ5rE-unsplash.jpg"
             alt={name}
-            width={250}
-            height={250}
+            width={500}
+            height={500}
           />
           <div>
             <h1>Title: {name.toUpperCase()}</h1>
